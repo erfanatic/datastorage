994368026e9ee41b7d9669efc1849140a97a0fbf
erfanatic <erfan.mahmud@gmail.com>
2016-08-20 22:59:29 +0600

    This is the first random update on GIT from Android Studio with a new class named UpdateData ironically.

---
+++ app/src/main/java/com/erfanatic/datastorageexperiment/database/UpdateData.java
@@ -0,0 +1,9 @@
+package com.erfanatic.datastorageexperiment.database;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by erfan on 20-Aug-16.
+ */
+public class UpdateData  {
+}
