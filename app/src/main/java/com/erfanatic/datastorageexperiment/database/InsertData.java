4edb2c8204cf39205d083f6a814eab990037529a
erfanatic <erfan.mahmud@gmail.com>
2016-08-22 20:47:54 +0600

    This is the first random update on GIT from Android Studio with a new class named UpdateData ironically.

--- app/src/main/java/com/erfanatic/datastorageexperiment/database/InsertData.java
+++ app/src/main/java/com/erfanatic/datastorageexperiment/database/InsertData.java
@@ -3,6 +3,7 @@ package com.erfanatic.datastorageexperiment.database;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -14,7 +15,7 @@ import com.erfanatic.datastorageexperiment.R;
  * Created by erfan on 16-Aug-16.
  */
 public class InsertData extends AppCompatActivity {
-
+    Toolbar toolbar;
     private EditText username;
     private EditText password;
     private DbAdapter myDbHelper;
@@ -22,6 +23,10 @@ public class InsertData extends AppCompatActivity {
     protected void onCreate( Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_database);
+        // Initializing and setting up the toolbar
+        toolbar= (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
 
         username = (EditText) findViewById(R.id.editText_username);
         password = (EditText) findViewById(R.id.editText_password);
